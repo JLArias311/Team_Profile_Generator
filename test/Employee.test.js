c3f0ec95d7cd0f1d73e864b8a2cb537234e2ab21
Jonathan Arias <JonathanL.Arias@outlook.com>
2020-10-28 17:22:18 -0400

    Created Employee Class to pass all tests

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -13,39 +13,46 @@ test("Can set name via constructor arguments", () => {
   const e = new Employee(name);
   expect(e.name).toBe(name);
 });
+// Passed
 
 test("Can set id via constructor argument", () => {
   const testValue = 100;
   const e = new Employee("Foo", testValue);
   expect(e.id).toBe(testValue);
 });
+// Passed
 
 test("Can set email via constructor argument", () => {
   const testValue = "test@test.com";
   const e = new Employee("Foo", 1, testValue);
   expect(e.email).toBe(testValue);
 });
+// Passed
 
 test("Can get name via getName()", () => {
   const testValue = "Alice";
   const e = new Employee(testValue);
   expect(e.getName()).toBe(testValue);
 });
+// Passed
 
 test("Can get id via getId()", () => {
   const testValue = 100;
   const e = new Employee("Foo", testValue);
   expect(e.getId()).toBe(testValue);
 });
+// Passed
 
 test("Can get email via getEmail()", () => {
   const testValue = "test@test.com";
   const e = new Employee("Foo", 1, testValue);
   expect(e.getEmail()).toBe(testValue);
 });
+// Passed
 
 test("getRole() should return \"Employee\"", () => {
   const testValue = "Employee";
   const e = new Employee("Alice", 1, "test@test.com");
   expect(e.getRole()).toBe(testValue);
 });
+// What are the slashes around the string "Employee" for?
