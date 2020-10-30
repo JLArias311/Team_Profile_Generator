f97f5be35b739721951cc9cb9b743f1c691cf713
Jonathan Arias <JonathanL.Arias@outlook.com>
2020-10-30 17:53:59 -0400

    Finsihed Draft(1)

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
+// Creates a variable that holds an absolute path for team.html that will be created in writeFile
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-// Need to figure out what these are
 
 const render = require("./lib/htmlRenderer");
 
@@ -17,159 +17,171 @@ const idArray = [];
 
 class Team {
     // Defining part of prompt that gathers manager's information and starts prompt
-createManager() {
-    console.log("Please build your team");
-    return inquirer.prompt([
-        {
-            type: "input",
-            name: "managerName",
-            message: "What is your manager's name?"
-        },
-        {
-            type: "number",
-            name: "managerId",
-            message: "What is your manager's id?"
-        },
-        {
-            type: "input",
-            name: "managerEmail",
-            message: "What is your manager's email?"
-        },
-        {
-            type: "number",
-            name: "managerOfficeNumber",
-            message: "What is your manager's office number?"
-        },
-    ])
-    .then(() => {
-        this.createTeam();
-    });
-    // Once manger information is input the prompt will continue on to the createTeam function
-}
+    createManager() {
+        console.log("Please build your team");
+        return inquirer.prompt([
+            {
+                type: "input",
+                name: "managerName",
+                message: "What is your manager's name?"
+            },
+            {
+                type: "input",
+                name: "managerId",
+                message: "What is your manager's id?"
+            },
+            {
+                type: "input",
+                name: "managerEmail",
+                message: "What is your manager's email?"
+            },
+            {
+                type: "input",
+                name: "managerOfficeNumber",
+                message: "What is your manager's office number?"
+            },
+        ])
+            .then(val => {
+                // Creating new Manager object with the value's from the prompt filling in it's parameters
+                const managerObj = new Manager(val.managerName, val.managerId, val.managerEmail, val.managerOfficeNumber);
 
-// Defining part of prompt that gathers team member's information
-createTeam() {
-    return inquirer.prompt([
-        {
-            type: "list",
-            name: "memberType",
-            message: "Which type of team member would you like to add?",
-            choices: ["Engineer", "Intern", "I don't want to add any more team members"]
-        }
-    ])
-        .then(answer => {
-            console.log(answer.memberType);
-            switch (answer.memberType) {
-                case "Engineer":
-                    this.engineerInfo();
-                    break;
-                case "Intern":
-                    this.internInfo();
-                    break;
-                case "I don't want to add anymore team members":
-                    this.done();
+                // console.log(managerObj);
+
+                // Pushing created manager object into teamMembers array
+                teamMembers.push(managerObj);
+
+                // console.log(teamMembers);
+
+                // Once manger information is input the prompt will continue on to the createTeam function
+                this.createTeam();
+            });
+    }
+
+
+
+
+    // Method that prompts user to select employee type. This method leads prompt to continue on to .engineerInfo(), ,intertInfo(), or the done()
+    createTeam() {
+        return inquirer.prompt([
+            {
+                type: "list",
+                name: "memberType",
+                message: "Which type of team member would you like to add?",
+                choices: ["Engineer", "Intern", "I don't want to add any more team members"]
             }
-        });
+        ])
+            .then(answer => {
+                console.log(answer.memberType);
+                switch (answer.memberType) {
+                    case "Engineer":
+                        this.engineerInfo();
+                        break;
+                    case "Intern":
+                        this.internInfo();
+                        break;
+                    case "I don't want to add anymore team members":
+                        this.done()
+                        break;
+                    default:
+                        this.done();
+                }
+            });
+    }
+
+    // If Engineer is slected we run these prompts and then loop back to the createTeam function
+    engineerInfo() {
+        return inquirer.prompt([
+            {
+                type: "input",
+                name: "nameEngineer",
+                message: "What is your engineer's name?",
+            },
+            {
+                type: "input",
+                name: "idEngineer",
+                message: "What is your engineer's id?",
+            },
+            {
+                type: "input",
+                name: "emailEngineer",
+                message: "What is your engineer's email?",
+            },
+            {
+                type: "input",
+                name: "github",
+                message: "What is your engineer's GitHub username?",
+            },
+        ])
+            .then(val => {
+                // Creating new Engineer object
+                const engineerObj = new Engineer(val.nameEngineer, val.idEngineer, val.emailEngineer, val.github);
+
+                // Pushing to array
+
+                teamMembers.push(engineerObj);
+
+                // starting createTeam() again
+
+                this.createTeam();
+            });
+    }
+
+    // If Intern is slected we run these prompts and then loop back to the createTeam function
+    internInfo() {
+        return inquirer.prompt([
+            {
+                type: "input",
+                name: "nameIntern",
+                message: "What is your intern's name?",
+            },
+            {
+                type: "input",
+                name: "idIntern",
+                message: "What is your intern's id?",
+            },
+            {
+                type: "input",
+                name: "emailIntern",
+                message: "What is your intern's email?",
+            },
+            {
+                type: "input",
+                name: "school",
+                message: "What is your intern's school?",
+            },
+        ])
+            .then(val => {
+                const internObj = new Intern(val.nameIntern, val.idIntern, val.emailIntern, val.school);
+
+                teamMembers.push(internObj);
+
+                this.createTeam();
+            });
+    }
+
+    // Closes inquirer prompt
+    done() {
+        // Renders html files to house teamMembers array's values
+        const actualizeTeam = render(teamMembers);
+
+        // Writes a new html with all rendered html files stitched together
+        fs.writeFileSync(outputPath, actualizeTeam, "utf8");
+
+        console.log("Generating HTML...");
+
+        // exits
+        process.exit(0);
+    }
+
+
+
 }
 
-// If Engineer is slected we run these prompts and then loop back to the createTeam function
-engineerInfo() {
-    return inquirer.prompt([
-        {
-            type: "input",
-            name: "nameEngineer",
-            message: "What is your engineer's name?",
-        },
-        {
-            type: "input",
-            name: "idEngineer",
-            message: "What is your engineer's id?",
-        },
-        {
-            type: "input",
-            name: "emailEngineer",
-            message: "What is your engineer's email?",
-        },
-        {
-            type: "input",
-            name: "engineerGithub",
-            message: "What is your engineer's GitHub username?",
-        },
-    ])
-        .then(() => {
-            this.createTeam();
-        });
-}
-
-// If Intern is slected we run these prompts and then loop back to the createTeam function
-internInfo() {
-    return inquirer.prompt([
-        {
-            type: "input",
-            name: "nameIntern",
-            message: "What is your intern's name?",
-        },
-        {
-            type: "input",
-            name: "idIntern",
-            message: "What is your intern's id?",
-        },
-        {
-            type: "input",
-            name: "emailIntern",
-            message: "What is your intern's email?",
-        },
-        {
-            type: "input",
-            name: "internSchool",
-            message: "What is your intern's school?",
-        },
-    ])
-        .then(() => {
-            this.createTeam();
-        });
-}
-
-// Closes inquirer prompt
-// Need to add gnerateHtml function
-done() {
-    console.log("Generating HTML...");
-    process.exit(0);
-}
-
-
-}
-
-// Can just call all functions in an init()
-
+// Creating new Team object that contains all object's function
 const team = new Team();
 
+// Starts object's methods
 team.createManager();
 
 
 
-
-
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team mem
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
